d75314e1c64e470d3f7d1385d03c5a7456162bb5
ravi_sharma <ravi.sharma26494@gmail.com>
2022-07-21 15:34:06 +0530

    Adding routes and exact directory

--- calculator.js
+++ calculator.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
-app.get('/',(req,res)=>{
-    res.send('Hello this will be a calculator')
+// app.get('/',(req,res)=>{
+//     res.send('Hello this will be a calculator')
+// })
+//instead of using the above code now what i want to do is to send a whole file index.html
+app.get('/',(req, res)=>{
+    //res.sendFile('index.html');
+    // instead of using the relative path use thuis which will give the directory of the the file
+    //console.log(__dirname);
+    res.sendFile(__dirname +'/index.html')
 })
+
 app.listen(3000, ()=>{
     console.log('Server started on port 3000');
 })
