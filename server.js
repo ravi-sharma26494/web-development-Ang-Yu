f53b1c68d7cf88b2ea7f0791a807a5d7e284e73a
ravi_sharma <ravi.sharma26494@gmail.com>
2022-07-21 14:30:14 +0530

    Adding diffrent routes

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/',(request, response)=>{  // this is method by express to handle get r
     // response.send('Hello');
     response.send('<h1>Can you seet it ??</h1><p style="color:red;">Oka i can see it<p>');
 }); 
+
+// Targeting different route than the home/root
+app.get('/contact', (req, res)=>{
+    res.send('Contact me at: ravi.me@gmail.com')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('<ul><li>My name is Ravi Sharma</li><li>I am trying to learn Express</li></ul>')
+});
+
 app.listen(3000,()=>{
     console.log('server started on port 3000');
 });
